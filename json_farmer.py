2ecf5c4697a4dc560a406e2e69d8fa272e40c45b
waso <sblankenship2507@sdsu.edu>
2025-03-04 21:59:07 -0800

    JSON farmer extracts html into key value pairs

---
+++ json_farmer.py
@@ -0,0 +1,113 @@
+from bs4 import BeautifulSoup
+import re
+import time
+
+def soup_to_text(span):
+    return " ".join(cleaner(span.text).split())
+
+def cleaner(s):
+    return re.sub(r'[\u200e|\u200f]', ' ', s)
+def tbody_to_dict(tbody):
+    # table body
+    d = {}
+    # table row
+    for tr in tbody.find_all("tr"):
+        try:
+            # table header
+            th = tr.find("th")
+            if th is not None:
+                td = tr.find("td")
+                d[soup_to_text(th)] = soup_to_text(td)
+                continue
+
+            tds = [td for td in tr.find_all("td")]
+            if len(tds) != 2:
+                continue
+            d[soup_to_text(tds[0])] = soup_to_text(tds[1])
+        except Exception:
+            pass
+    return d
+
+def filter_divs(html):
+    info_json = {}
+    soup = BeautifulSoup(html, "lxml")
+
+# Example: Keep only the first half of <div> elements
+    # divs = soup.find_all("div")
+    main_container = soup.find(id="dp-container")
+
+    if main_container is None:
+        print("Not a product")
+        return
+    right_col = main_container.find(id="rightCol" )
+    center_col = main_container.find(id="centerCol")
+    left_col = main_container.find(id = "leftCol")
+    print(len(right_col.find_all('div')))
+    print(len(center_col.find_all('div')))
+    print(len(left_col.find_all('div')))
+    p_title = center_col.find(id="productTitle")
+    rating = center_col.find(id="acrPopover")
+
+    print(p_title.prettify())
+    # table information
+    tables = center_col.find_all('tbody')
+    
+    t_dicts = [tbody_to_dict(table) for table in tables]
+    print(t_dicts)
+    try:
+        product_facts_container = center_col.find(id="productFactsDesktopExpander")
+
+        details_title = product_facts_container.find(class_='product-facts-title')
+        sibling_list = details_title.find_next_sibling()
+
+        for list_item in sibling_list.find_all('li'):
+            spans = [soup_to_text(span) for span in list_item.find_all("span", class_="a-color-base")]
+            info_json[spans[0]] = spans[1]
+    except Exception:
+        pass
+
+
+    # depth card table
+    depth_cards = center_col.find_all(id=re.compile(r"^depthInfoCard_\d+$"))
+
+    # for c in depth_cards:
+        # print(c)
+    # print(depth_cards[0].prettify())
+    print(f"N depth cards: {len(depth_cards)}")
+
+    product_details = main_container.find_all(id='detailBullets_feature_div')
+    for details in product_details:
+        for detail in details.find_all(class_ = 'a-list-item'):
+            spans = [soup_to_text(span) for span in detail.find_all("span")]
+            info_json[spans[0]] = spans[1]
+
+    print(info_json)
+
+    try:
+        prod_details = soup.find(id="prodDetails")
+
+        tables = prod_details.find_all('tbody')
+        
+        t_dicts = [tbody_to_dict(table) for table in tables]
+        print(t_dicts)
+    except Exception:
+        pass
+
+    # print(soup.find(id="topHighlights"))
+    # print(soup.prettify())
+
+
+# Print or use the filtered HTML
+
+def create_json():
+    pass
+
+html = None
+with open('Skillet.html', 'r') as file:
+    html = file.read()
+s = time.time()
+filter_divs(html)
+e = time.time()
+
+
+print(f'Time: {e-s}')
--- scraper.py
+++ scraper.py
@@ -3,15 +3,16 @@ from crawl4ai import *
 import time
 import requests
 import json
+import pickle
 
 print('running')
 async def main():
 # await url request
     print('run')
     s = time.time()
-    hosts = get_urls()
-    urls = [
-        "https://www.amazon.com/s?k=skillets&crid=J4HH9K8GQZDO&sprefix=skillet%2Caps%2C171"
+    # hosts = get_urls()
+    hosts = [
+        ("amazon", ["https://www.amazon.com/Hellmanns-Mayonnaise-Condiment-Squeeze-Cage-Free/dp/B091DCC2CH?dib=eyJ2IjoiMSJ9.4ONmHaMWTq1ZHhlXvbCPGauj7swH_JzX45bcACzKBNxV9jmJWPnOyc0b1v46CGHp9OaXikCORs_0qdCVjxScTdiGMAAVSfxlf78w0ngh2q4gGYWEdvjbRBjyZY_8TbhNdZ4In4kBidwii9_p5rxKJZ_sypJe0GRQpdXspjbOIXPZd2qcWWAq4OKMZg4m7MJ34PQRlBGI0bFvyZchtyocDqSwmv7xOJjfYB0Za_dyhRdYt87C8EtQ_JlaOivsyc4j9KzxdBIGlaBSMCy_s7yEpiXp5x3nhX6yIFXUDjXmcHCq4mG-bQyDUoBekapedtjJJbcLYNZGF1nMpLM5JdZN-sGgvoPf2ZaqV9KNsrd6e_jIWDIRPXgeQyPcYwteJ8nV7OWNe0v0tWmEVJ_0LfWg6CoQ0XPDUl963RjRqnij3g5otb394BSUoag1y8En_uGG.fyatwkxFfAD1tAHciWfqTR7MmYkmSQzyWpDvY2JIZOo&dib_tag=se&keywords=mayonnaise&qid=1741154067&sr=8-5"])
     ]
     # convert to 2d list, where each list has urls from different hosts
     url_bursts = []
@@ -26,19 +27,28 @@ async def main():
         hostnames.append(hostname)
 
     htmls = []
-    async with AsyncWebCrawler() as crawler:
+    browser_config = BrowserConfig(
+        headless=True,
+        viewport_width=1920,
+        viewport_height=1080,
+        browser_type = "firefox"
+
+    )
+    async with AsyncWebCrawler(config=browser_config) as crawler:
         for urls in url_bursts:
             # fetch all urls in burst simultaneously
-            htmls += list(await asyncio.gather(*[scrape_url(crawler, url) for url in urls]))
-            print(htmls)
-
+            htmls += await scrape_urls(crawler, urls)
+            print(len(htmls))
+    
+    with open("Skillet.html", 'w') as file:
+        file.write(htmls[0])
     # send htmls
-    send_htmls(htmls)
+    # send_htmls(htmls)
 
     e = time.time()
     print(f'Time: {e-s}')
     
-server_url = 'http://3.220.232.5:5000'
+server_url = 'http://34.232.48.247:5000'
 def get_urls():
     get_url = f'{server_url}/getUrls'
     r = requests.get(url = get_url)
@@ -59,17 +69,31 @@ def send_htmls(htmls):
 
 
 # Function to scrape a list of URLs in sequence
-async def scrape_url(crawler, url):
+async def scrape_urls(crawler, urls):
     run_config = CrawlerRunConfig(
         verbose=False,
-        check_robots_txt=False
+        log_console = True,
+        check_robots_txt=False,
+        delay_before_return_html=1.0,
+        # wait_for_images=True,
+        word_count_threshold=1,
+
+        scan_full_page = True,
+        magic=False,
+        remove_overlay_elements=True,
+        screenshot=True,
+
+
     )
-    result = await crawler.arun(url=url, config=run_config)
+    results = await crawler.arun_many(urls=urls, config=run_config)
     # You can process the result here (e.g., save to a file or print it)
-    print(f"Scraped: {url}")
-    print(type(result))
+    for res in results:
+        if res.success:
+            print(f"{res.url} successfully crawled")
+        else:
+            print(f"Failed to crawl {res.url}")
     # print(result)
-    return result.html
+    return [res.html for res in results if res.success]
 
 
 
